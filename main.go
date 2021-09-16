448e12295fbe768658eed0ad6a029f6c276944c0
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-17 08:03:00 +0900

    39

--- main.go
+++ main.go
@@ -1,39 +1,13 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"sync"
+	"time"
 )
 
-var wg sync.WaitGroup
-
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	queue := make(chan string)
-	for i := 0; i < 2; i++ {
-		wg.Add(1)
-		go fetchURL(ctx, queue)
-	}
-
-	queue <- "https://www.example.com"
-	queue <- "https://www.example.net"
-	queue <- "https://www.example.net/foo"
-	queue <- "https://www.example.net/bar"
-
-	cancel()
-	wg.Wait()
-}
-
-func fetchURL(ctx context.Context, queue chan string) {
+	defer fmt.Println("exited")
 	for {
-		select {
-		case <-ctx.Done():
-			fmt.Println("worker exit")
-			wg.Done()
-			return
-		case url := <-queue:
-			fmt.Println("fetching", url)
-		}
+		time.Sleep(1 * time.Second)
 	}
 }

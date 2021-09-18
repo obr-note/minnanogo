6fe78cfd7b5b03afff946f0d85b3f77ef9aa160b
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-18 09:14:22 +0900

    4-1

--- main.go
+++ main.go
@@ -1,39 +1,13 @@
 package main
 
 import (
-	"context"
+	"flag"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
-	defer fmt.Println("done")
-	trapSignals := []os.Signal{
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT,
-	}
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, trapSignals...)
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		sig := <-sigCh
-		fmt.Println("Got signal", sig)
-		cancel()
-	}()
-	doMain(ctx)
-}
-
-func doMain(ctx context.Context) {
-	defer fmt.Println("done doMain")
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-		}
-	}
+	const defaultPort int = 3000
+	var port = flag.Int("port", defaultPort, "use")
+	flag.Parse()
+	fmt.Printf("%v", *port)
 }

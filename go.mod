3751d5a167eae3b16bb6b105ecfbd87b545e3619
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-18 10:22:34 +0900

    4-24

---
+++ go.mod
@@ -0,0 +1,5 @@
+module minnanogo
+
+go 1.16
+
+require github.com/mitchellh/cli v1.1.2
--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
+
+	"github.com/mitchellh/cli"
 )
 
+type AddCommand struct{}
+
+func (c *AddCommand) Synopsis() string {
+	return "Add todo task to list"
+}
+
+func (c *AddCommand) Help() string {
+	return "Usage: todo add [option]"
+}
+
+func (c *AddCommand) Run(args []string) int {
+	fmt.Println("あざます")
+	return 0
+}
+
 func main() {
-	const defaultPort int = 3000
-	var port = flag.Int("port", defaultPort, "use")
-	flag.Parse()
-	fmt.Printf("%v", *port)
+	c := cli.NewCLI("todo", "0.1.0")
+
+	c.Args = os.Args[1:]
+
+	c.Commands = map[string]cli.CommandFactory{
+		"add": func() (cli.Command, error) {
+			return &AddCommand{}, nil
+		},
+	}
+
+	exitCode, err := c.Run()
+	if err != nil {
+		fmt.Printf("Failed to execute: %s¥n", err.Error())
+	}
+
+	os.Exit(exitCode)
 }

ee138ab4e59e0cf127cd46d9cc0c497dff36cd24
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-18 16:50:25 +0900

    5.2-2

--- main.go
+++ main.go
@@ -12,8 +12,13 @@ type Point struct {
 
 func main() {
 	p := &Point{X: 10, Y: 5}
-	rv := reflect.ValueOf(p)
-	fmt.Printf("type = %v\n", rv.Type())
-	fmt.Printf("kind = %v\n", rv.Kind())
+	rv := reflect.ValueOf(p).Elem()
+	// fmt.Printf("type = %v\n", rv.Type())
+	// fmt.Printf("kind = %v\n", rv.Kind())
 	fmt.Printf("interface = %v\n", rv.Interface())
+
+	xv := rv.Field(0)
+	fmt.Printf("xv = %d\n", xv.Int())
+	xv.SetInt(100)
+	fmt.Printf("xv (after) = %d\n", xv.Int())
 }

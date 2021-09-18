57ce570338ec0f1eb65a4ff5ce7e4ab38798a0e2
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-18 16:33:13 +0900

    5.2

--- main.go
+++ main.go
@@ -1,38 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
-	"os"
-	"strings"
-	"testing"
+	"reflect"
 )
 
-const Version = "1.0.0"
+type Point struct {
+	X int
+	Y int
+}
 
 func main() {
-	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
-	os.Exit(cli.Run(os.Args))
-}
-
-type CLI struct {
-	outStream, errStream io.Writer
-}
-
-func (c *CLI) Run(args []string) int {
-	fmt.Fprintf(c.errStream, "gobook version %s \n", Version)
-	return 0
-}
-
-func TestRun(t *testing.T) {
-	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
-	cli := &CLI{outStream: outStream, errStream: errStream}
-	args := strings.Split("gobook -version", " ")
-
-	cli.Run(args)
-	expected := fmt.Sprintf("gobook version %s", Version)
-	if !strings.Contains(outStream.String(), expected) {
-		t.Errorf("expected %q to eq %q", outStream.String(), expected)
-	}
+	p := &Point{X: 10, Y: 5}
+	rv := reflect.ValueOf(p)
+	fmt.Printf("type = %v\n", rv.Type())
+	fmt.Printf("kind = %v\n", rv.Kind())
+	fmt.Printf("interface = %v\n", rv.Interface())
 }

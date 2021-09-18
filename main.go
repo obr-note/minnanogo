da48236202660961ebe6e330645b2cf225947b8f
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-19 01:54:57 +0900

    5.3-2

--- main.go
+++ main.go
@@ -5,27 +5,47 @@ import (
 )
 
 func main() {
-	filename := "./test"
-	f, err := os.Open(filename)
-	if err != nil {
-		return
-	}
-	defer f.Close()
+	os.Exit(Run())
+}
 
-	ch := make(chan []byte)
-	go func() {
-		defer close(ch)
+func Run() int {
+	files := []string{"./test0", "./test1", "./test2"}
+
+	ch1 := make(chan []byte, 3)
+	ch2 := make(chan []byte, 3)
+	ch3 := make(chan []byte, 3)
+	chs := []chan []byte{ch1, ch2, ch3}
+	// chs := make([]chan []byte, 3)
+
+	fc := func(i int, f *os.File) {
+		defer close(chs[i])
 		buf := make([]byte, 4096)
-		for {
+		for io := 0; io < 3; io++ {
 			n, err := f.Read(buf)
 			if err != nil {
 				return
 			}
-			ch <- buf[:n]
+			chs[i] <- buf[:n]
 		}
-	}()
+	}
 
-	for in := range ch {
-		os.Stdout.Write(in)
+	for i, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			return 1
+		}
+		defer f.Close()
+		go fc(i, f)
+	}
+
+	var data []byte
+	for {
+		select {
+		case data = <-chs[0]:
+		case data = <-chs[1]:
+		case data = <-chs[2]:
+		}
+
+		os.Stdout.Write(data)
 	}
 }

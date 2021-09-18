417559c3cd64b1f1f118f0e1cc7e0b99054ff541
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-18 19:02:44 +0900

    5.3-1

--- main.go
+++ main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
+	"os"
 )
 
-type Point struct {
-	X int
-	Y int
-}
-
 func main() {
-	p := &Point{X: 10, Y: 5}
-	rv := reflect.ValueOf(p).Elem()
-	// fmt.Printf("type = %v\n", rv.Type())
-	// fmt.Printf("kind = %v\n", rv.Kind())
-	fmt.Printf("interface = %v\n", rv.Interface())
+	filename := "./test"
+	f, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	defer f.Close()
 
-	xv := rv.Field(0)
-	fmt.Printf("xv = %d\n", xv.Int())
-	xv.SetInt(100)
-	fmt.Printf("xv (after) = %d\n", xv.Int())
+	ch := make(chan []byte)
+	go func() {
+		defer close(ch)
+		buf := make([]byte, 4096)
+		for {
+			n, err := f.Read(buf)
+			if err != nil {
+				return
+			}
+			ch <- buf[:n]
+		}
+	}()
+
+	for in := range ch {
+		os.Stdout.Write(in)
+	}
 }

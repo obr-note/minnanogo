5071e42b28fd675dc679842fcfe94998154870fb
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-20 15:48:54 +0900

    6-2

--- test/calc_test.go
+++ test/calc_test.go
@@ -16,3 +16,13 @@ func ExampleHello() {
 	fmt.Println("Hello")
 	// Output: Hellol
 }
+
+func ExampleUnordered() {
+	for _, v := range []int{1, 2, 3} {
+		fmt.Println(v)
+	}
+	// Unordered output:
+	// 2
+	// 3
+	// 1
+}

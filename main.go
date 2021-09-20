abf278fd58af680886c1f1ce657317110cf45beb
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-20 17:00:49 +0900

    6-5

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
+	var mux sync.Mutex
 	c := make(chan bool)
 	m := make(map[string]string)
 	go func() {
+		mux.Lock()
 		m["1"] = "a"
+		mux.Unlock()
 		c <- true
 	}()
+	mux.Lock()
 	m["2"] = "b"
+	mux.Unlock()
 	<-c
 	for k, v := range m {
 		fmt.Println(k, v)

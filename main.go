7d923bbce8558703f1a904e7db177be57b53a6aa
obr-note <k33mdukb9n@yahoo.co.jp>
2021-09-20 16:59:17 +0900

    6-4

--- main.go
+++ main.go
@@ -1,24 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-// type IntSlice []int
-
-// func (p IntSlice) Len() int {
-// 	return len(p)
-// }
-// func (p IntSlice) Less(i, j int) bool {
-// 	return p[i] < p[j]
-// }
-// func (p IntSlice) Swap(i, j int) {
-// 	p[i], p[j] = p[j], p[i]
-// }
+import "fmt"
 
 func main() {
-	numbers := []int{10, 8, 2, 5, 1, 3, 4, 9, 7, 6}
-	sort.Sort(sort.IntSlice(numbers))
-	fmt.Println(numbers)
+	c := make(chan bool)
+	m := make(map[string]string)
+	go func() {
+		m["1"] = "a"
+		c <- true
+	}()
+	m["2"] = "b"
+	<-c
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
